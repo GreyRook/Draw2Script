faab94f9bab12114c567449b318bdb1a204ab052
Mike Barkmin <mbarkmin@gmail.com>
2015-07-17 12:52:09 +0200

    error handling for "overflow" and "nothing is selected"

--- host/draw2script.jsx
+++ host/draw2script.jsx
@@ -1,7 +1,7 @@
-﻿//Minified JSON2
+//Minified JSON2
 "object"!=typeof JSON&&(JSON={}),function(){"use strict";function f(t){return 10>t?"0"+t:t}function this_value(){return this.valueOf()}function quote(t){return rx_escapable.lastIndex=0,rx_escapable.test(t)?'"'+t.replace(rx_escapable,function(t){var e=meta[t];return"string"==typeof e?e:"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+t+'"'}function str(t,e){var r,n,o,u,f,a=gap,i=e[t];switch(i&&"object"==typeof i&&"function"==typeof i.toJSON&&(i=i.toJSON(t)),"function"==typeof rep&&(i=rep.call(e,t,i)),typeof i){case"string":return quote(i);case"number":return isFinite(i)?String(i):"null";case"boolean":case"null":return String(i);case"object":if(!i)return"null";if(gap+=indent,f=[],"[object Array]"===Object.prototype.toString.apply(i)){for(u=i.length,r=0;u>r;r+=1)f[r]=str(r,i)||"null";return o=0===f.length?"[]":gap?"[\n"+gap+f.join(",\n"+gap)+"\n"+a+"]":"["+f.join(",")+"]",gap=a,o}if(rep&&"object"==typeof rep)for(u=rep.length,r=0;u>r;r+=1)"string"==typeof rep[r]&&(n=rep[r],o=str(n,i),o&&f.push(quote(n)+(gap?": ":":")+o));else for(n in i)Object.prototype.hasOwnProperty.call(i,n)&&(o=str(n,i),o&&f.push(quote(n)+(gap?": ":":")+o));return o=0===f.length?"{}":gap?"{\n"+gap+f.join(",\n"+gap)+"\n"+a+"}":"{"+f.join(",")+"}",gap=a,o}}var rx_one=/^[\],:{}\s]*$/,rx_two=/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,rx_three=/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,rx_four=/(?:^|:|,)(?:\s*\[)+/g,rx_escapable=/[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,rx_dangerous=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;"function"!=typeof Date.prototype.toJSON&&(Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+f(this.getUTCMonth()+1)+"-"+f(this.getUTCDate())+"T"+f(this.getUTCHours())+":"+f(this.getUTCMinutes())+":"+f(this.getUTCSeconds())+"Z":null},Boolean.prototype.toJSON=this_value,Number.prototype.toJSON=this_value,String.prototype.toJSON=this_value);var gap,indent,meta,rep;"function"!=typeof JSON.stringify&&(meta={"\b":"\\b","	":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},JSON.stringify=function(t,e,r){var n;if(gap="",indent="","number"==typeof r)for(n=0;r>n;n+=1)indent+=" ";else"string"==typeof r&&(indent=r);if(rep=e,e&&"function"!=typeof e&&("object"!=typeof e||"number"!=typeof e.length))throw new Error("JSON.stringify");return str("",{"":t})}),"function"!=typeof JSON.parse&&(JSON.parse=function(text,reviver){function walk(t,e){var r,n,o=t[e];if(o&&"object"==typeof o)for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(n=walk(o,r),void 0!==n?o[r]=n:delete o[r]);return reviver.call(t,e,o)}var j;if(text=String(text),rx_dangerous.lastIndex=0,rx_dangerous.test(text)&&(text=text.replace(rx_dangerous,function(t){return"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})),rx_one.test(text.replace(rx_two,"@").replace(rx_three,"]").replace(rx_four,"")))return j=eval("("+text+")"),"function"==typeof reviver?walk({"":j},""):j;throw new SyntaxError("JSON.parse")})}();
 
-//Array of Base64 characters
+﻿//Array of Base64 characters
 var BASE_64 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
                "V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
                "q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+",
@@ -24,10 +24,13 @@ var cropBox;
 
 var hexColor = false;
 
-generatePixiJson();
-
 function generatePixiJson() {
 	var codeString = generateCreateJS({hexColor: true});
+	if(codeString == "No objects are selected") {
+		return codeString;
+	} else if(codeString.indexOf("overflow") > -1) {
+		return codeString;
+	}
 	var type = "graphics";
 	var data = [];
 	var json = {
@@ -35,16 +38,16 @@ function generatePixiJson() {
 	};
 	var splitDot = codeString.split(".");
 	for(var i = 0; i < splitDot.length; i++) {
-        if(splitDot[i].length != 0) {
-         var splitBracket = splitDot[i].split(/[(]+/);
-		var cmd = splitBracket[0];
-		var args = splitBracket[1].substring(0,splitBracket[1].length-1).replace(/'/g, "").split(",");
-		var dataItem = {
-				"cmd": cmd,
-				"args": args,
+		if(splitDot[i].length != 0) {
+			var splitBracket = splitDot[i].split(/[(]+/);
+			var cmd = splitBracket[0];
+			var args = splitBracket[1].substring(0,splitBracket[1].length-1).replace(/'/g, "").split(",");
+			var dataItem = {
+					"cmd": cmd,
+					"args": args,
+			}
+			data.push(dataItem);
 		}
-		data.push(dataItem);
-        }
 	}
 	json.data = data;
 	return JSON.stringify(json,null, 2);
@@ -65,7 +68,12 @@ function generateCreateJS(params) {
 		}
 	}
 	instruction += ".cp()";
-	return instruction;
+	if(objects.length == 0) {
+		return "select: No objects are selected";
+	} else if(instruction.indexOf("overflow") > -1) {
+		return "overflow: Please scale your graphic down. Only up to 131071 Pixel are supported.";
+	}
+ 	return instruction;
 	
 }
 
